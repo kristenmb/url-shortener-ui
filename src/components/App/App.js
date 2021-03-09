c9c009ec6fe70e05c005d79a7c1b8f697dc7879e
Kristen Bair <kristenmbair@gmail.com>
2021-03-09 11:10:20 -0700

    Add error handling for bad api responses

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -15,26 +15,32 @@ export class App extends Component {
 
   componentDidMount() {
     getUrls()
-      .then(urls => this.setState({ urls: urls.urls }))
+      .then(urls => this.setState({ 
+        urls: urls.urls,
+        error: ''
+      }))
       .catch(error => this.setState({ error: "Cannot retrieve your shortened urls, please try again later." }))
   }
 
   submitNewUrl = (submission) => {
     addUrl(submission)
-      .then(response => this.setState({ urls: [...this.state.urls, response ]}))
+      .then(response => this.setState({ 
+        urls: [...this.state.urls, response ],
+        error: '' 
+      }))
       .catch(error => this.setState({ error: "Url shortening unsuccessful, please try again." }))
   }
 
   render() {
     return (
-      <main className="App">
-        <header>
-          <h1>URL Shortener</h1>
-          <UrlForm submitNewUrl={this.submitNewUrl}/>
-        </header>
+        <main className="App">
+          <header>
+            <h1>URL Shortener</h1>
+            <UrlForm submitNewUrl={this.submitNewUrl}/>
+          </header>
 
-        <UrlContainer urls={this.state.urls}/>
-      </main>
+          <UrlContainer urls={this.state.urls} error={this.state.error}/>
+        </main>
     );
   }
 }
--- src/components/UrlContainer/UrlContainer.js
+++ src/components/UrlContainer/UrlContainer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './UrlContainer.css';
 
-const UrlContainer = ({ urls }) => {
+const UrlContainer = ({ urls, error }) => {
   const urlEls = urls.map(url => {
     return (
       <div className="url" key={url.id}>
@@ -14,7 +14,9 @@ const UrlContainer = ({ urls }) => {
 
   return (
     <section>
-      { urlEls.length ? urlEls : <p>No urls yet! Find some to shorten!</p> }
+      {!error && urlEls.length && urlEls}
+      { !urlEls.length && !error && <p>No urls yet! Find some to shorten!</p> }
+      { error && <p>{error}</p> }
     </section>
   )
 }
