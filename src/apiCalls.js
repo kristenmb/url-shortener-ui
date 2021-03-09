c810746649bc82de9d0cfa8507607abc35d08563
Kristen Bair <kristenmbair@gmail.com>
2021-03-09 09:40:25 -0700

    Create post fetch call

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,3 +2,16 @@ export const getUrls = () => {
   return fetch('http://localhost:3001/api/v1/urls')
       .then(response => response.json())
 }
+
+export const addUrl = (submission) => {
+  return fetch('http://localhost:3001/api/v1/urls', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({...submission}), 
+  })
+    .then(response => {
+      console.log(response.status)
+      return response.json()})
+}
